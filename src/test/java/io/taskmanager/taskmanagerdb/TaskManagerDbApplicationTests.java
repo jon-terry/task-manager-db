88d30720d4483a74a5972013239bf3293d390cf6
Jon <jet8468@gmail.com>
2026-04-25 20:58:43 -0400

    Initial project setup

---
+++ src/test/java/io/taskmanager/taskmanagerdb/TaskManagerDbApplicationTests.java
@@ -0,0 +1,13 @@
+package io.taskmanager.taskmanagerdb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskManagerDbApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
